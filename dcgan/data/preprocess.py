660f139f8c3321d0d44597e311773c1be0fa5527
Julia Bullard <5534@holbertonstudents.com>
2023-10-31 18:28:43 -0500

    Data

---
+++ dcgan/data/preprocess.py
@@ -0,0 +1,16 @@
+import numpy as np
+from keras.datasets import mnist
+
+
+def load_data_set():
+    (X_train, _), (X_test, _) = mnist.load_data()
+    X = np.vstack((X_train, X_test))
+    X = X.astype('float 32')
+    
+    # Normalize: [-1, 1]
+    X = (X - 127.5) / 127.5
+    
+    # Normalize to [0, 1]
+    # X = X/255.0
+
+    return X
